695da2bb5cb466fc53a9fb24d46e05fae083e025
Sage Bain <bain.sage@student.greenriver.edu>
2024-01-16 13:49:38 -0500

    Added default route

--- index.php
+++ index.php
@@ -17,7 +17,11 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function() {
-    echo 'diner';
+    //echo 'diner';
+
+    //Display a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 //Run Fat-Free
