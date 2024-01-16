093191cfd9c6eaacd09b6e665ed73c306191b97d
Sage Bain <bain.sage@student.greenriver.edu>
2024-01-16 12:56:22 -0500

    Added Controller

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
-echo 'diner';
+/* Sage Bain
+ * January 2024
+ * http://sbain.greenriverdev.com/328/diner/
+ * This is my CONTROLLER for the Diner app
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate the Fat-Free Framework (F3)
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo 'diner';
+});
+
+//Run Fat-Free
+$f3->run(); //instance method
+
+
+
+
