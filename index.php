90832536877ea38aa97235f586bce7197439793a
Sage Bain <bain.sage@student.greenriver.edu>
2024-02-08 13:14:58 -0500

    Added data validation to orders

--- index.php
+++ index.php
@@ -10,8 +10,9 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Require the autoload file
-require_once('vendor/autoload.php');
-require_once('model/data-layer.php');
+require_once ('vendor/autoload.php');
+require_once ('model/data-layer.php');
+require_once ('model/validate.php');
 
 //Instantiate the Fat-Free Framework (F3)
 $f3 = Base::instance();
@@ -46,17 +47,30 @@ $f3->route('GET /summary', function() {
 // Define the order1 route
 $f3->route('GET|POST /order1', function($f3) {
 
+    // Initialize variables
+    $food = "";
     // If the form has been posted
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
         // Validate the data
-        $food = $_POST['food'];
+        if(validFood($_POST['food']))
+        {
+            $food = $_POST['food'];
+        }
+        else {
+            $f3->set('errors["food"]', "Invalid food");
+        }
+
         $meal = $_POST['meal'];
-        // Put the data in the session array
-        $f3->set('SESSION.food', $food);
-        $f3->set('SESSION.meal', $meal);
-        // Redirect to order2 route
-        $f3->reroute('order2');
+        // If there are no errors
+        if(empty($f3->get('errors')))
+        {
+            // Put the data in the session array
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
+            // Redirect to order2 route
+            $f3->reroute('order2');
+        }
     }
 
     // Get data from the model and add to the F3 "hive"
